92f0d18262edaa93572ee93661b8c46d9d61da31
andrzul <aineken93@gmail.com>
2022-04-20 08:30:52 +0200

    change number 2

--- src/App.js
+++ src/App.js
@@ -5,32 +5,78 @@ import {datas} from "./sounds"
 function App() {
 
   const[volume, setVolume] = useState(1)
+  const[recording , setRecording] = useState("")
+
+  const playRecording=()=>{
+
+    let index = 0;
+    let recordArray = recording.split(" ")
+    console.log(recordArray);
+    console.log(recording);
+   const interval =  setInterval(() => {
+  var triggerMusic = document.getElementById(recordArray[index])
+
+      triggerMusic.volume = volume;
+      triggerMusic.currentTime = 0;
+      triggerMusic.play();
+      index++;
+
+ }, 300);
+
+ setTimeout(()=>{
+ clearInterval(interval)
+
+ }, 300*recordArray.length-1)
+
+
+
+  }
   
   return (
 
 
 <div className="bg-info min-vh-100 text-white">
 <div className="text-center">
-<h2>Drum Machine</h2>
+<h1>Drum Machine</h1>
     {datas.map((data)=>{
-      return <Pad key={data.id} data={data} volume={volume}/>
+      return <Pad key={data.id} data={data} volume={volume} setRecording={setRecording}/>
     })}
 
-<input type="range" className='w-50' 
+<h2>Volume</h2>
+<input type="range" className='w-' 
 step="0.01"
 min="0"
 max="1"
 onChange={(e)=>{setVolume(e.target.value)}}
 value={volume}
 />
+<h3>{recording}</h3>
+
+{recording&&(
+<>
+<button onClick={playRecording} className="btn btn-success m-3">
+  PLAY
+</button>
+<button onClick={()=>setRecording("") } className="btn btn-danger m-3">
+  CLEAR
+  </button>
+
+</>
+
+)}
+
+
 </div>
-</div>
+</div> 
 
   );
 }
 
 
-function Pad({data,volume}){
+function Pad({data,volume, setRecording}){
+
+  const[active, setActive] = useState(false);
+
 
 useEffect(()=>{
 document.addEventListener("keydown", handleKeyPress)
@@ -38,9 +84,8 @@ document.addEventListener("keydown", handleKeyPress)
 return ()=>{
   document.removeEventListener("keydown", handleKeyPress)
 }
-})
+});
 
- 
 const handleKeyPress = (e)=>{
 if (e.keyCode=== data.keyCode){
   playMusic();
@@ -49,16 +94,19 @@ if (e.keyCode=== data.keyCode){
 
 
 const playMusic=()=>{
-
+  setActive(true);
+  setTimeout(()=>setActive(false),200)
   var triggerMusic = document.getElementById(data.keyTrigger)
-      triggerMusic.volume = volume;
-      triggerMusic.play();
 
+      triggerMusic.volume = volume;
+      triggerMusic.currentTime = 0;
+      triggerMusic.play();
+  setRecording( prev=>prev+data.keyTrigger+" ")
 }
 
   return(
     <>
-        <div onClick={playMusic} className="btn btn-secondary p-3 m-4 ">
+        <div onClick={playMusic} className={`btn btn-primary p-4 m-3 ${active && 'btn-warning'} `}>
           <audio src={data.url} id={data.keyTrigger}></audio>
           {data.keyTrigger}
         </div>
