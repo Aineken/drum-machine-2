7e5b8993cdc09650a36bcb9c7a108ef3b4b7ba0c
andrzul <aineken93@gmail.com>
2022-04-20 07:49:54 +0200

    drum-machine-test-2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import {datas} from "./sounds"
 
 function App() {
+
+  const[volume, setVolume] = useState(1)
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+<div className="bg-info min-vh-100 text-white">
+<div className="text-center">
+<h2>Drum Machine</h2>
+    {datas.map((data)=>{
+      return <Pad key={data.id} data={data} volume={volume}/>
+    })}
+
+<input type="range" className='w-50' 
+step="0.01"
+min="0"
+max="1"
+onChange={(e)=>{setVolume(e.target.value)}}
+value={volume}
+/>
+</div>
+</div>
+
   );
+}
+
+
+function Pad({data,volume}){
+
+useEffect(()=>{
+document.addEventListener("keydown", handleKeyPress)
+
+return ()=>{
+  document.removeEventListener("keydown", handleKeyPress)
+}
+})
+
+ 
+const handleKeyPress = (e)=>{
+if (e.keyCode=== data.keyCode){
+  playMusic();
+}
+}
+
+
+const playMusic=()=>{
+
+  var triggerMusic = document.getElementById(data.keyTrigger)
+      triggerMusic.volume = volume;
+      triggerMusic.play();
+
+}
+
+  return(
+    <>
+        <div onClick={playMusic} className="btn btn-secondary p-3 m-4 ">
+          <audio src={data.url} id={data.keyTrigger}></audio>
+          {data.keyTrigger}
+        </div>
+    </>
+  )
 }
 
 export default App;
